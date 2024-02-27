c8bb47ed5830d9e3758406d37892a1932215d3cb
rahul patel <rkpatelmahadev@gmail.com>
2024-02-27 23:48:12 +0530

    first

---
+++ minipro.js
@@ -0,0 +1,18 @@
+function fun(){
+    let contact=document.getElementById("AboutUs").value;
+  window.location.assign("Aboutus.html");
+  }
+   
+  function myadmission(){
+    let myabout=document.getElementById("AboutUs").value;
+  window.location.assign("Admission.html");
+  }
+   
+function myAcademics(){
+  let myAcademic=document.getElementById("Academics").value;
+  window.location.assign("Acadmatic.html");
+}
+function studentlife(){
+  let studentlife=document.getElementById("student").value;
+  window.location.assign("student.html");
+}
